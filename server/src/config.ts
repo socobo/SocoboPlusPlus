a57d20d93af6583ebe0b68a71f4744764b8ab5d0
flashback2k14 <feedbackcalcest@gmail.com>
2016-12-23 18:19:32 +0100

    #20 - commit before delete passport stuff

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,5 +3,6 @@ export class Config {
   static DB_PW: string = "socobo";
   static DATABASE_URL: string = `postgres://${Config.DB_USER}:${Config.DB_PW}@localhost:5432/socobo?ssl=false`;
   static PORT: number = 8282;
-  static NODE_ENV: string = "development"; 
+  static NODE_ENV: string = "development";
+  static TOKEN_SECRET: string = "!*sdkfduf487387478478hhuhudf#28458//(/)##+993";
 }
---
+++ server/src/logic/utils/cryptoUtils.ts
@@ -0,0 +1,38 @@
+import * as bcrypt from "bcrypt";
+import { ApiError } from './../../models/apiError';
+
+
+export class CryptoUtils {
+
+  static hashPassword (userPassword: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+          return reject(new ApiError("Error by Generating the Salt value.", 
+                                      CryptoUtils.name, 
+                                      "hashPassword(userPassword)", err));
+        }
+        bcrypt.hash(userPassword, salt, (err, hash) => {
+            if (err) {
+              return reject(new ApiError("Error by Generating the hashed Password.", 
+                                          CryptoUtils.name, 
+                                          "hashPassword(userPassword)", err));
+            }
+            resolve(hash);
+        });
+      });
+    });
+  }
+
+  static comparePasswords (firstPw: string, secondPw: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(firstPw, secondPw, (err, isMatch) => {
+        if (err) {
+          return reject(new ApiError("Error by comparing Passwords.", CryptoUtils.name, 
+                                      "comparePasswords(firstPw,secondPw)", err));
+        }
+        resolve(isMatch);
+      });
+    });
+  }
+}
