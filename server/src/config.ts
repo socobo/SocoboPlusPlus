ea6d5b6cce4a964b14e2e93e3ccff025691102d1
flashback2k14 <feedbackcalcest@gmail.com>
2016-12-24 12:33:27 +0100

    #22 - logging and database configuration based on environment

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,6 +1,8 @@
 export class Config {
   static DB_USER: string = "postgres";
   static DB_PW: string = "socobo";
+  static DATABASE_URL_TEST: string = `postgres://${Config.DB_USER}:${Config.DB_PW}@localhost:5432/socobo_test?ssl=false`;
+  static DATABASE_URL_DEV: string = `postgres://${Config.DB_USER}:${Config.DB_PW}@localhost:5432/socobo_dev?ssl=false`;
   static DATABASE_URL: string = `postgres://${Config.DB_USER}:${Config.DB_PW}@localhost:5432/socobo?ssl=false`;
   static PORT: number = 8282;
   static NODE_ENV: string = "development";
