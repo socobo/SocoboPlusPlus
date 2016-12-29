4181a8d42628f451c9745d743582606910d58d63
hetec <hebner.tec@gmail.com>
2016-12-29 20:47:58 +0100

    Remove old version of validation method

--- server/src/middleware/validator.ts
+++ server/src/middleware/validator.ts
@@ -7,17 +7,14 @@ export class ApiValidator{
 
     validate<T>(type: {new(): T}, req: Request): Promise<any>{
         return new Promise<any>((resolve: any, reject: any) => {
-            console.log("BODY", req.body);
-            
             let obj: any = req.body;
             let objectToValidate = new type();
+
             for(let prop in obj){
                 (objectToValidate as any)[prop] = obj[prop];
             }          
             
             validate(objectToValidate).then(errors => {
-                console.log("OBJ:", objectToValidate);
-                
                 if(errors.length > 0){
                     resolve(new ValidationError('Validation failed',
                     "Validator", 'validate()', errors).forResponse())
@@ -30,32 +27,4 @@ export class ApiValidator{
             })
         })
     }
-
-    apiValidator<T>(type: {new(): T}){
-        return ((req: Request, res: Response, next:NextFunction) => {
-            console.log("BODY", req.body);
-            
-            let obj: any = req.body;
-            let objectToValidate = new type();
-            for(let prop in obj){
-                (objectToValidate as any)[prop] = obj[prop];
-            }          
-            
-            validate(objectToValidate).then(errors => {
-                console.log("OBJ:", objectToValidate);
-                
-                if(errors.length > 0){
-                    res.status(400).json(
-                        new ValidationError('Validation failed',
-                    "Validator", 'validate()', errors).forResponse())
-                }else{
-                    next();
-                }
-            }).catch(error => {
-                res.status(500).json(new ValidationError('Internal Server Error',
-                "Validator", 'validate()', error).forResponse())
-            }) 
-        })
-    }
-
 }
