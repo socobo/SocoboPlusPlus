411fe1c5b91073cd0c274c10bdd22884d81d6a7c
flashback2k14 <feedbackcalcest@gmail.com>
2016-12-31 10:01:03 +0100

    #20 - delete AuthUtils, add token check to AuthValidator, inject AuthValidator into User and Logs route

--- server/src/logic/middleware/authValidator.ts
+++ server/src/logic/middleware/authValidator.ts
@@ -1,5 +1,7 @@
+import * as jwt from "jsonwebtoken"; 
 import { Request, Response, NextFunction } from "express";
 import { ApiError } from "./../../models/apiError";
+import { Config } from "./../../config";
 
 
 export class AuthValidator {
@@ -11,7 +13,7 @@ export class AuthValidator {
 
       if (!hasEmailProperty && !hasUsernameProperty) {
         return reject(new ApiError("Request Body doesn't have a Username or Email Address!", AuthValidator.name, 
-                                    "post('/login')", new Error("No Username or Email provided!")).forResponse());
+                                    "checkRequest(req)", new Error("No Username or Email provided!")).forResponse());
       }
 
       if (hasEmailProperty) {
@@ -24,4 +26,39 @@ export class AuthValidator {
     });
   }
 
+  /**
+   * Rewrite as Promise Middleware --> checkValidToken()
+   */
+  checkValidToken (req: Request): Promise<any> {
+    return new Promise((resolve, reject) => {
+
+      const authHeader: string = (process.env.TOKEN_HEADER || Config.TOKEN_HEADER);
+      const token = req.body.token 
+                    || req.query.token 
+                    || req.headers[authHeader];
+
+      if (token) {
+        jwt.verify(token, 
+                    (process.env.TOKEN_SECRET || Config.TOKEN_SECRET), 
+                    (err: any, decoded: any)  => {
+
+          if (err) {
+            let msg = "Failed to authenticate token.";
+            if (err.name === "TokenExpiredError") {
+              msg = `${msg} Token expired at ${err.expiredAt}`;
+            }
+            if (err.name === "JsonWebTokenError") {
+              msg = `${msg} Error message: ${err.message}`;
+            }
+            return reject(new ApiError(msg, AuthValidator.name, "checkValidToken(req)", err));
+          }
+          
+          req.body.decoded = decoded;
+          resolve();
+        });
+      } else {
+        return reject(new ApiError("No token provided.", AuthValidator.name, "checkValidToken(req)", new Error()));
+      }
+    });
+  }
 }
--- server/src/routes/api/v1/logs/index.ts
+++ server/src/routes/api/v1/logs/index.ts
@@ -1,15 +1,26 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { LogService } from "./../../../../logic/services/logging.service";
-import { AuthUtils } from "./../../../../logic/utils/authUtils";
+import { AuthValidator } from "./../../../../logic/middleware/authValidator";
+import { ApiError } from "./../../../../models/apiError";
 
 
 export class LogsRouteV1 {
-  constructor (private _router: Router) {}
+  constructor (
+    private _router: Router,
+    private _authValidator: AuthValidator
+  ) {}
 
   createRoutes (): Router {
     // get all errors
-    this._router.get("/errors", AuthUtils.checkState, (req: Request, res: Response, next: NextFunction) => {
-      res.status(200).json(LogService.getErrors());
+    this._router.get("/errors", 
+      (req: Request, res: Response, next: NextFunction) => {
+        this._authValidator.checkValidToken(req)
+          .then(() => next())
+          .catch((err: ApiError) => res.status(400).json(err));
+      }, 
+      (req: Request, res: Response, next: NextFunction) => {
+
+        res.status(200).json(LogService.getErrors());
     });
 
     // return Router to use in server.ts
