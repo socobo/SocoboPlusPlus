5970b6d01c0833280950951a1ba2747e13e4eee5
hetec <hebner.tec@gmail.com>
2017-01-10 07:47:50 +0100

    Create error messages
    A simple const to store all error messages in one place. This could also simply replaced if we want to change the language

---
+++ server/src/error-messages.ts
@@ -0,0 +1,10 @@
+export const ERROR_MESSAGES: any = {
+	"user.not.found": "User could not be found",
+	"auth.wrong.password": "Authentication failed. Wrong password",
+	"auth.used.email.or.password": "Email or Username is already registered. Please use another one.",
+	"internal.error": "Internal server error",
+	"internal.no.password.hash": "No password hash",
+	"internal.error.create.salt": "Error while generating the Salt value",
+	"internal.error.create.hash": "Error while generating password hash",
+	"internal.error.request.body": "Something went wrong with extracting request body"
+}
