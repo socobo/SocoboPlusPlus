ff0a68cc042fd0bca1918d59e6b0359c8d3dd8be
flashback2k14 <feedbackcalcest@gmail.com>
2017-01-14 16:43:40 +0100

    #28 - add date field and add functions to SocoboUser Model Object

--- server/src/models/database/socobo-user.ts
+++ server/src/models/database/socobo-user.ts
@@ -7,4 +7,48 @@ export class SocoboUser {
   hasTermsAccepted: boolean; 
   isAdmin: boolean; 
   provider: string;
+  created: number;
+  lastModified: number;
+
+  addUsername = (username: string): this => {
+    this.username = username;
+    return this;
+  }
+
+  addEmail = (email: string): this => {
+    this.email = email;
+    return this;
+  }
+
+  addPassword = (password: string): this => {
+    this.password = password;
+    return this;
+  }
+
+  addImage = (image: string): this => {
+    this.image = image;
+    return this;
+  }
+
+  addHasTermsAccepted = (hasTermsAccepted: boolean): this => {
+    this.hasTermsAccepted = hasTermsAccepted;
+    return this;
+  }
+
+  addIsAdmin = (isAdmin: boolean): this => {
+    this.isAdmin = isAdmin;
+    return this;
+  }
+
+  addProvider = (provider: string): this => {
+    this.provider = provider;
+    return this;
+  }
+
+  addDates = (): this => {
+    const now = Date.now();
+    this.created = now;
+    this.lastModified = now;
+    return this;
+  }
 }
