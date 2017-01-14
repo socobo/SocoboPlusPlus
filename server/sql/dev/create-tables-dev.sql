c481fedef9ce387131b236c3c56d2f432252d476
flashback2k14 <feedbackcalcest@gmail.com>
2017-01-14 17:01:57 +0100

    #28 - extends SQL scripts and add Constraints to Socobo_User table

--- server/sql/dev/create-tables-dev.sql
+++ server/sql/dev/create-tables-dev.sql
@@ -2,11 +2,13 @@
 
 CREATE TABLE Socobo_User (
   ID SERIAL PRIMARY KEY,
-  username VARCHAR,
-  email VARCHAR,
-  password VARCHAR,
-  image VARCHAR,
-  hasTermsAccepted BOOLEAN,
-  isAdmin BOOLEAN,
-  provider VARCHAR
+  username VARCHAR NOT NULL UNIQUE,
+  email VARCHAR NOT NULL UNIQUE,
+  password VARCHAR NOT NULL,
+  image VARCHAR NOT NULL,
+  hasTermsAccepted BOOLEAN NOT NULL,
+  isAdmin BOOLEAN NOT NULL,
+  provider VARCHAR NOT NULL,
+  created NUMERIC NOT NULL,
+  lastModified NUMERIC NOT NULL CHECK (lastModified >= created)
 );
