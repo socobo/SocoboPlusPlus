8a9006c09217851da6392af6436e245174c9a705
flashback2k14 <feedbackcalcest@gmail.com>
2017-01-19 20:48:00 +0100

    #38 - fix log.routes.ts

--- server/src/routes/api/v1/logs/log.routes.ts
+++ server/src/routes/api/v1/logs/log.routes.ts
@@ -1,9 +1,10 @@
-import { Response, Request, Router, NextFunction } from "express";
-import { LogService } from "./../../../../logic/services/index";
+import { NextFunction, Request, Response, Router } from "express";
 import { AuthValidator } from "./../../../../logic/middleware/index";
+import { LogService } from "./../../../../logic/services/index";
 import { ApiError } from "./../../../../models/index";
 
 export class LogRoute {
+
   constructor (
     private _router: Router,
     private _authValidator: AuthValidator
@@ -11,15 +12,17 @@ export class LogRoute {
 
   public createRoutes (): Router {
     // get all errors
-    this._router.get("/errors", (req: Request, res: Response, next: NextFunction) => {
+    this._router.get("/errors",
+      (req: Request, res: Response, next: NextFunction) => {
         this._authValidator.checkValidToken(req)
           .then(() => next())
           .catch((err: any) => res.status(err.statusCode).json(err.forResponse()));
-        }, (req: Request, res: Response, next: NextFunction) => {
+        }, 
+      (req: Request, res: Response, next: NextFunction) => {
           res.status(200).json(LogService.getErrors());
         }
     );
     // return Router to use in server.ts
     return this._router;
   }
-};
+}
