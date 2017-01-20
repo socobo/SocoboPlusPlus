570e20a642c524e0b0a4572a5bbb970698a4fef2
flashback2k14 <feedbackcalcest@gmail.com>
2017-01-20 17:18:55 +0100

    #38 - fix utils lint errors

--- server/src/logic/utils/crypto.utils.ts
+++ server/src/logic/utils/crypto.utils.ts
@@ -1,28 +1,26 @@
 import * as bcrypt from "bcrypt";
-import { 
-  ApiError, ComparePwResult, SocoboUser 
+import {
+  ApiError, ComparePwResult, ERRORS, SocoboUser
 } from "./../../models/index";
-import { ERRORS } from "./../../models/index"
-
 
 export class CryptoUtils {
 
-  hashPassword (userPassword: string): Promise<string> {
+  public hashPassword (userPassword: string): Promise<string> {
     return new Promise((resolve, reject) => {
-      bcrypt.genSalt(10, (err, salt) => {
-        if (err) {
-          let e = new ApiError(ERRORS.AUTH_SALT_GENERATION)
+      bcrypt.genSalt(10, (errSalt, salt) => {
+        if (errSalt) {
+          const e = new ApiError(ERRORS.AUTH_SALT_GENERATION)
             .addSource(CryptoUtils.name)
             .addSourceMethod("hashPassword(..)")
-            .addCause(err);
+            .addCause(errSalt);
           return reject(e);
         }
-        bcrypt.hash(userPassword, salt, (err, hash) => {
-            if (err) {
-              let e = new ApiError(ERRORS.AUTH_PW_HASH_GENERATION)
+        bcrypt.hash(userPassword, salt, (errHash, hash) => {
+            if (errHash) {
+              const e = new ApiError(ERRORS.AUTH_PW_HASH_GENERATION)
                 .addSource(CryptoUtils.name)
                 .addSourceMethod("hashPassword(..)")
-                .addCause(err);
+                .addCause(errHash);
               return reject(e);
             }
             resolve(hash);
@@ -31,11 +29,11 @@ export class CryptoUtils {
     });
   }
 
-  comparePasswords (firstPw: string, user: SocoboUser): Promise<ComparePwResult> {
+  public comparePasswords (firstPw: string, user: SocoboUser): Promise<ComparePwResult> {
     return new Promise((resolve, reject) => {
       bcrypt.compare(firstPw, user.password, (err, isMatch) => {
         if (err) {
-          let e = new ApiError(ERRORS.AUTH_PW_MISSMATCH)
+          const e = new ApiError(ERRORS.AUTH_PW_MISSMATCH)
             .addSource(CryptoUtils.name)
             .addSourceMethod("comparePasswords(..)")
             .addCause(err);
