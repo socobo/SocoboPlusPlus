87407151050b742440af996477a028287d5181d0
flashback2k14 <feedbackcalcest@gmail.com>
2017-02-01 18:26:23 +0100

    #34 - add _sendErrorResponse() function

--- server/src/logic/handler/validation/auth-validation.handler.ts
+++ server/src/logic/handler/validation/auth-validation.handler.ts
@@ -9,36 +9,40 @@ export class AuthValidationHandler {
   public checkRequest = (req: Request, res: Response, next: NextFunction): void => {
     this._authValidationMiddleware.checkRequest(req)
       .then(() => next())
-      .catch((e: any) => res.status(e.statusCode).json(e.forResponse()));
+      .catch((e: any) => this._sendErrorResponse(e, res));
   }
 
   public extractBody = (req: Request, res: Response, next: NextFunction): void => {
     this._authValidationMiddleware.extractRequestBody(req)
       .then(() => next())
-      .catch((e: any) => res.status(e.statusCode).json(e.forResponse()));
+      .catch((e: any) => this._sendErrorResponse(e, res));
   }
 
   public checkToken = (req: Request, res: Response, next: NextFunction): void => {
     this._authValidationMiddleware.checkValidToken(req)
       .then(() => next())
-      .catch((e: any) => res.status(e.statusCode).json(e.forResponse()));
+      .catch((e: any) => this._sendErrorResponse(e, res));
   }
 
   public checkUser = (req: Request, res: Response, next: NextFunction): void => {
     this._authValidationMiddleware.checkValidUser(req)
       .then(() => next())
-      .catch((e: any) => res.status(e.statusCode).json(e.forResponse()));
+      .catch((e: any) => this._sendErrorResponse(e, res));
   }
 
   public restricted = (req: Request, res: Response, next: NextFunction): void => {
     this._authValidationMiddleware.checkUserRoleForRestriction(req, true)
       .then(() => next())
-      .catch((e: any) => res.status(e.statusCode).json(e.forResponse()));
+      .catch((e: any) => this._sendErrorResponse(e, res));
   }
 
   public nonRestricted = (req: Request, res: Response, next: NextFunction): void => {
     this._authValidationMiddleware.checkUserRoleForRestriction(req, false)
       .then(() => next())
-      .catch((e: any) => res.status(e.statusCode).json(e.forResponse()));
+      .catch((e: any) => this._sendErrorResponse(e, res));
+  }
+
+  private _sendErrorResponse = (e: any, res: Response) => {
+    res.status(e.statusCode).json(e.forResponse());
   }
 }
