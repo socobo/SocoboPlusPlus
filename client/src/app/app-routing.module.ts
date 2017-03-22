b234a3e4e91901aa6b471b80b68522c53535d290
flashback2k14 <feedbackcalcest@gmail.com>
2017-03-22 20:43:53 +0100

    #66 - bugfix tests, move and rename root to app

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Route } from "@angular/router";
 
-// Components
-import { RootComponent } from "./components/root/root.component";
-
 const ROOT_ROUTES: Route[] = [
   {
     path: "recipes",
