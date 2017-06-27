328a353a358d46976eb452c4020cc46a78c4c6e2
hetec <hebner.tec@gmail.com>
2017-06-27 21:19:52 +0200

    #76 Create Recipe Step Model

--- server/src/models/database/recipe-step.ts
+++ server/src/models/database/recipe-step.ts
@@ -1,13 +1,65 @@
+import { IsNotEmpty, IsNumber, Length, IsString, MinLength } from "class-validator";
+import { ValidationGroup } from "./../enums/validation-group";
+
 export class RecipeStep {
   public id: number;
-  public step: number;
-  public description: string;
+  @IsNotEmpty({
+    groups: [ ValidationGroup.RECIPE ]
+  })
+  @IsNumber({
+    groups: [ ValidationGroup.RECIPE ]
+  })
+  public recipeId: number;
+  @IsNotEmpty({
+    groups: [ ValidationGroup.RECIPE ]
+  })
+  @Length(1, 50, {
+    groups: [ ValidationGroup.RECIPE ]
+  })
+  public stepTitle: string;
+  @IsNotEmpty({
+    groups: [ ValidationGroup.RECIPE ]
+  })
+  @IsNumber({
+    groups: [ ValidationGroup.RECIPE ]
+  })
+  public stepNumber: number;
+  @IsNotEmpty({
+    groups: [ ValidationGroup.RECIPE ]
+  })
+  @IsNumber({
+    groups: [ ValidationGroup.RECIPE ]
+  })
+  @MinLength(1, {
+    groups: [ ValidationGroup.RECIPE ]
+  })
+  public stepDescription: string;
+  public createdDate: number;
+  public lastModifiedDate: number;
 
-  public addStep (step: number) {
-    this.step = step;
+
+  public step (stepNumber: number) {
+    this.stepNumber = stepNumber;
+    return this;
   }
 
-  public addDescription (description: string) {
-    this.description = description;
+  public description (stepDescription: string) {
+    this.stepDescription = stepDescription;
+    return this;
+  }
+
+  public title (stepTitle: string) {
+    this.stepTitle = stepTitle;
+    return this;
+  }
+
+  public created (createdDate: number) {
+    this.createdDate = createdDate;
+    return this;
+  }
+
+  public lastModified (lastModifiedDate: number) {
+    this.lastModifiedDate = lastModifiedDate;
+    return this;
   }
 }
