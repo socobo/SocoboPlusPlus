432402399ee131c944bdd18eb79f3fd4854f00db
flashback2k14 <feedbackcalcest@gmail.com>
2017-08-27 16:15:20 +0200

    #85 - bugfix model-validation tests, simplify validateObject()

--- server/src/app/middleware/model-validation.middleware.ts
+++ server/src/app/middleware/model-validation.middleware.ts
@@ -10,32 +10,20 @@ export class ModelValidationMiddleware {
    * @param req
    * @param validationGroups
    */
-  public validateObject (objToValidate: Validatable, req: Request, validationGroups: ValidationGroup[]): Promise<any> {
-    return new Promise<any>((resolve: any, reject: any) => {
-      const obj: any = req.body;
-      const mappedGroups: string[] = validationGroups.map((group) => group.toString());
-
-      objToValidate.clone(obj);
-
-      validate(objToValidate, { groups: mappedGroups })
-        .then((errors: any[]) => {
-          if (errors.length > 0) {
-            const e = new ValidationError(ERRORS.VAL_INVALID_INPUT)
-              .addSource(ModelValidationMiddleware.name)
-              .addSourceMethod("validateObject(..)")
-              .addValidationErrors(errors);
-            reject(e);
-          } else {
-            resolve();
-          }
-        })
-        .catch((error: any) => {
-          const e = new ValidationError(ERRORS.VAL_INVALID_INPUT)
-            .addSource(ModelValidationMiddleware.name)
-            .addSourceMethod("validateObject(..)")
-            .addCause(error);
-          reject(e);
-        });
-    });
+  public validateObject = async (objToValidate: Validatable, req: Request,
+                                 validationGroups: ValidationGroup[]): Promise<void> => {
+    // create new object for validation
+    objToValidate.clone(req.body);
+    // get validation groups
+    const mappedGroups = validationGroups.map((group: ValidationGroup) => group.toString());
+    // validate newly created object
+    const result = await validate(objToValidate, {groups: mappedGroups });
+    // throw error if the object is not valid
+    if (result.length > 0) {
+      throw new ValidationError(ERRORS.VAL_INVALID_INPUT)
+        .addSource(ModelValidationMiddleware.name)
+        .addSourceMethod("validateObject(..)")
+        .addValidationErrors(result);
+    }
   }
 }
--- server/test/3-model-validation.middleware.test.ts
+++ server/test/3-model-validation.middleware.test.ts
@@ -1,91 +1,73 @@
+/*tslint:disable:no-unused-expression*/
+
 process.env["NODE_ENV"] = "test";
 
 import * as chai from "chai";
-import * as chaiAsPromised from "chai-as-promised";
 import { NextFunction, Request, Response, Router } from "express";
 import * as mocha from "mocha";
 import * as sinon from "sinon";
 import { ModelValidationMiddleware, ValidationGroup } from "./../src/app/index";
 import { Recipe } from "./../src/recipe/index";
 
-chai.use(chaiAsPromised);
 chai.should();
 
 describe("Model Validation Middleware", () => {
 
+  const expect = chai.expect;
   const mocks = require("node-mocks-http");
 
-  it("validation of a recipe with empty title and userId failes with two properties", () => {
+  it("validation of a recipe rejected with correct validation error values", async () => {
 
-    let req: any;
     const recipe: Recipe = new Recipe();
     recipe.title = "";
 
-    req = mocks.createRequest({
+    const req = mocks.createRequest({
       body: recipe
     });
 
-    const prom: any = new ModelValidationMiddleware().validateObject(new Recipe(), req, [ValidationGroup.RECIPE]);
-    return prom.should.be.rejected.then((e: any) => {
-      e.should.to.have.deep.property("validationErrors").to.have.deep.lengthOf(2);
-    });
+    try {
+      const prom = await new ModelValidationMiddleware().validateObject(new Recipe(), req, [ValidationGroup.RECIPE]);
+    } catch (error) {
+      expect(error).to.have.deep.property("statusCode", 400);
+      expect(error).to.have.deep.property("message", "The provided input is invalid");
+      expect(error).to.have.deep.property("sourceMethod", "validateObject(..)");
+      expect(error).to.have.deep.property("source", "ModelValidationMiddleware");
+    }
   });
 
-  // FIXME: AssertionError: expected [ApiError: ... to have property 'validationErrors[0].property'
-  // it("validation of a recipe with empty title and userId failes with the properties title and userId", () => {
+  it("validation of a recipe with empty title and userId failes with the properties title and userId", async () => {
 
-  //   let req: any;
-  //   const recipe: Recipe = new Recipe();
-  //   recipe.title = "";
-
-  //   req = mocks.createRequest({
-  //     body: recipe
-  //   });
-
-  //   const prom: any = new ModelValidationMiddleware().validate(Recipe, req, [ValidationGroup.RECIPE]);
-  //   return prom.should.be.rejected.then((e: any) => {
-  //     console.log(e);
-  //     const valiErrors = e.validationErrors;
-  //     console.log(valiErrors);
-  //     e.should.to.have.deep.property("validationErrors");
-  //     e.should.to.have.deep.property("validationErrors[0].property", "title");
-  //     // e.should.to.have.deep.property("validationErrors[1].property", "userId");
-  //     // e.should.to.have.deep.property("validationErrors[2].property", "steps");
-  //   });
-  // });
-
-  it("validation of a recipe rejected with correct validation error values", () => {
-
-    let req: any;
     const recipe: Recipe = new Recipe();
     recipe.title = "";
 
-    req = mocks.createRequest({
+    const req = mocks.createRequest({
       body: recipe
     });
 
-    const prom: any = new ModelValidationMiddleware().validateObject(new Recipe(), req, [ValidationGroup.RECIPE]);
-    return prom.should.be.rejected.then((e: any) => {
-      e.should.to.have.deep.property("message", "The provided input is invalid");
-      e.should.to.have.deep.property("sourceMethod", "validateObject(..)");
-      e.should.to.have.deep.property("source", "ModelValidationMiddleware");
-    });
+    try {
+      const prom = await new ModelValidationMiddleware().validateObject(new Recipe(), req, [ValidationGroup.RECIPE]);
+    } catch (error) {
+      expect(error).to.have.deep.property("validationErrors");
+      expect(error.validationErrors).to.have.length(2);
+      expect(error.validationErrors[0]).to.have.deep.property("property", "title");
+      expect(error.validationErrors[1]).to.have.deep.property("property", "userId");
+    }
   });
 
-  // FIXME: Error: Invalid Chai property: resolved
-  // it("validation of a recipe with title and userId will be resolved", () => {
+  it("validation of a recipe with title and userId will be resolved", async () => {
 
-  //   let req: any;
-  //   const recipe: Recipe = new Recipe();
-  //   recipe.title = "Test Title";
-  //   recipe.userId = 2;
-  //   recipe.steps = [];
+    const recipe: Recipe = new Recipe();
+    recipe.title = "Test Title";
+    recipe.userId = 2;
+    recipe.steps = [];
 
-  //   req = mocks.createRequest({
-  //     body: recipe
-  //   });
+    const req = mocks.createRequest({
+      body: recipe
+    });
 
-  //   const prom: any = new ModelValidationMiddleware().validate(Recipe, req, [ValidationGroup.RECIPE]);
-  //   return prom.should.be.resolved;
-  // });
+    const result = await new ModelValidationMiddleware().validateObject(new Recipe(), req, [ValidationGroup.RECIPE]);
+    expect(result).to.be.undefined;
+  });
 });
+
+/*tslint:enable:no-unused-expression*/
