8f0713bd661ffb189ae13968d0b5ea1aab7dacfd
flashback2k14 <feedbackcalcest@gmail.com>
2017-09-10 19:51:07 +0200

    #85 - Bugfix Middleware

--- server/src/socobouser/middleware/SocoboUser.middleware.ts
+++ server/src/socobouser/middleware/SocoboUser.middleware.ts
@@ -4,6 +4,12 @@ import { SocoboUserUpdateType } from "../index";
 
 export class SocoboUserMiddleware {
 
+  private readonly MINUPDATETYPE = SocoboUserUpdateType.full;
+  private readonly MAXUPDATETYPE = SocoboUserUpdateType.terms;
+  private readonly MINLENGTH = SocoboUserUpdateType.full;
+  private readonly MAXLENGTHFULL = SocoboUserUpdateType.terms;
+  private readonly MAXLENGTHSINGLE = SocoboUserUpdateType.username;
+
   public checkUpdateType = (req: Request, res: Response, next: NextFunction): void => {
     const updateType: SocoboUserUpdateType = req.body.updateType;
     if (typeof updateType === "undefined") {
@@ -12,7 +18,7 @@ export class SocoboUserMiddleware {
       res.status(error.statusCode).json(error.forResponse());
       return;
     }
-    if (updateType < SocoboUserUpdateType.full || updateType > SocoboUserUpdateType.provider) {
+    if (updateType < this.MINUPDATETYPE || updateType > this.MAXUPDATETYPE) {
       const error = ErrorUtils.handleRequestError(ERRORS.USER_INVALID_UPDATE_TYPE,
         SocoboUserMiddleware.name, "checkUpdateType(..)");
       res.status(error.statusCode).json(error.forResponse());
@@ -39,7 +45,9 @@ export class SocoboUserMiddleware {
 
     switch (updateType) {
       case SocoboUserUpdateType.full:
-        if (fieldLength <= 0 || fieldLength > 6) { errors.push("Invalid Property length"); }
+        if (fieldLength <= this.MINLENGTH || fieldLength > this.MAXLENGTHFULL) {
+          errors.push("Invalid Property length");
+        }
         if (!fieldsToUpdate.hasOwnProperty("username")) { errors.push("Username is missing"); }
         if (!fieldsToUpdate.hasOwnProperty("email")) { errors.push("Email is missing"); }
         if (!fieldsToUpdate.hasOwnProperty("password")) { errors.push("Password is missing"); }
@@ -50,37 +58,51 @@ export class SocoboUserMiddleware {
         break;
 
       case SocoboUserUpdateType.username:
-        if (fieldLength <= 0 || fieldLength > 1) { errors.push("Invalid Property length"); }
+        if (fieldLength <= this.MINLENGTH || fieldLength > this.MAXLENGTHSINGLE) {
+          errors.push("Invalid Property length");
+        }
         if (!fieldsToUpdate.hasOwnProperty("username")) { errors.push("Username is missing"); }
         break;
 
       case SocoboUserUpdateType.email:
-        if (fieldLength <= 0 || fieldLength > 1) { errors.push("Invalid Property length"); }
+        if (fieldLength <= this.MINLENGTH || fieldLength > this.MAXLENGTHSINGLE) {
+          errors.push("Invalid Property length");
+        }
         if (!fieldsToUpdate.hasOwnProperty("email")) { errors.push("Email is missing"); }
         break;
 
       case SocoboUserUpdateType.password:
-        if (fieldLength <= 0 || fieldLength > 1) { errors.push("Invalid Property length"); }
+        if (fieldLength <= this.MINLENGTH || fieldLength > this.MAXLENGTHSINGLE) {
+          errors.push("Invalid Property length");
+        }
         if (!fieldsToUpdate.hasOwnProperty("password")) { errors.push("Password is missing"); }
         break;
 
       case SocoboUserUpdateType.image:
-        if (fieldLength <= 0 || fieldLength > 1) { errors.push("Invalid Property length"); }
+        if (fieldLength <= this.MINLENGTH || fieldLength > this.MAXLENGTHSINGLE) {
+          errors.push("Invalid Property length");
+        }
         if (!fieldsToUpdate.hasOwnProperty("imageUrl")) { errors.push("Image URL is missing"); }
         break;
 
       case SocoboUserUpdateType.role:
-        if (fieldLength <= 0 || fieldLength > 1) { errors.push("Invalid Property length"); }
+        if (fieldLength <= this.MINLENGTH || fieldLength > this.MAXLENGTHSINGLE) {
+          errors.push("Invalid Property length");
+        }
         if (!fieldsToUpdate.hasOwnProperty("role")) { errors.push("Role is missing"); }
         break;
 
       case SocoboUserUpdateType.provider:
-        if (fieldLength <= 0 || fieldLength > 1) { errors.push("Invalid Property length"); }
+        if (fieldLength <= this.MINLENGTH || fieldLength > this.MAXLENGTHSINGLE) {
+          errors.push("Invalid Property length");
+        }
         if (!fieldsToUpdate.hasOwnProperty("provider")) { errors.push("Provider is missing"); }
         break;
 
       case SocoboUserUpdateType.terms:
-        if (fieldLength <= 0 || fieldLength > 1) { errors.push("Invalid Property length"); }
+        if (fieldLength <= this.MINLENGTH || fieldLength > this.MAXLENGTHSINGLE) {
+          errors.push("Invalid Property length");
+        }
         if (!fieldsToUpdate.hasOwnProperty("hasTermsAccepted")) { errors.push("Has Terms Accepted is missing"); }
         break;
 
@@ -88,7 +110,7 @@ export class SocoboUserMiddleware {
         break;
     }
 
-    if (errors.length > 0) {
+    if (errors.length > this.MINLENGTH) {
       const error = ErrorUtils.handleValidationError(ERRORS.USER_INVALID_UPDATE_TYPE,
         SocoboUserMiddleware.name, "checkUpdateBody(..)", errors);
       res.status(error.statusCode).json(error.forResponse());
