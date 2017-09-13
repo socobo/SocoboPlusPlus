5038224f43c2c090592e6ea3d36aa11043edc637
flashback2k14 <feedbackcalcest@gmail.com>
2017-09-13 10:20:24 +0200

    #90 - Bugfix lint errors

--- server/src/db/implementation/mongo-db-extension.ts
+++ server/src/db/implementation/mongo-db-extension.ts
@@ -18,10 +18,10 @@ export class MongoDbExtension implements DbExtension {
   constructor (
     private _fooditemTemplateModel: Model<Document & FoodItemTemplate>,
     private _socoboUserModel: Model<Document & SocoboUser>,
-    private _recipeModel: Model<Document & Recipe>                                      
+    private _recipeModel: Model<Document & Recipe>
   ) {
     this.fooditemTemplate = new FoodItemTemplateRepository(_fooditemTemplateModel);
     this.socobouser = new SocoboUserRepository(_socoboUserModel);
-    this.recipe = new RecipeRepository(_recipeModel);                                     
+    this.recipe = new RecipeRepository(_recipeModel);
   }
 }
