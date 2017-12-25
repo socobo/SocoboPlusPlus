86431d640051837d46a0451e9d7854baddf0d006
flashback2k14 <feedbackcalcest@gmail.com>
2017-12-25 11:22:10 +0100

    #96 - add tests for admin and user

--- server/test/42-fooditemcategory.route.test.ts
+++ server/test/42-fooditemcategory.route.test.ts
@@ -1,11 +1,11 @@
-
 process.env["NODE_ENV"] = "test";
 
 import { expect } from "chai";
 import * as mocha from "mocha";
+import { SocoboUserRoleType } from "../src/socobouser/index";
 import { TestHelper } from "./helper/TestHelper";
 
-describe("FoodItemCategoryRoute - API v1", () => {
+describe("FoodItemCategoryRoute - API v1 - PASS AS ADMIN AND USER", () => {
 
   it("GET /api/v1/fooditemcategory should pass if a token is provided", async () => {
     const accessToken = await TestHelper.getToken();
@@ -41,8 +41,11 @@ describe("FoodItemCategoryRoute - API v1", () => {
       expect(result.body).to.deep.property("created");
       expect(result.body).to.deep.property("lastModified");
     });
+});
 
-  it("PUT /api/v1/fooditemcategory/:id should update the category and return the modified", async () => {
+describe("FoodItemCategoryRoute - API v1 - PASS AS ADMIN", () => {
+
+  it("PUT /api/v1/fooditemcategory/:id should update the category and return the modified as ADMIN", async () => {
     const id = "59a2f0667898dca760b01e56";
     const accessToken = await TestHelper.getToken();
     const resultBefore = await TestHelper.getAgent()
@@ -60,7 +63,7 @@ describe("FoodItemCategoryRoute - API v1", () => {
     expect(resultAfter.body.lastModified).to.not.equal(resultBefore.body.lastModified);
   });
 
-  it("POST /api/v1/fooditemcategory should save a new category and return it", async () => {
+  it("POST /api/v1/fooditemcategory should save a new category and return it AS ADMIN", async () => {
     const accessToken = await TestHelper.getToken();
     const result = await TestHelper.getAgent()
       .post(`/api/v1/fooditemcategory`)
@@ -83,5 +86,59 @@ describe("FoodItemCategoryRoute - API v1", () => {
       .del(`/api/v1/fooditemcategory/${id}`)
       .set("x-access-token", accessToken);
     expect(result.body.id).to.equal(id);
+  });
+});
+
+describe("FoodItemCategoryRoute - API v1 - FAIL AS USER", () => {
+
+  it("PUT /api/v1/fooditemcategory/:id should fail to update a category as USER", async () => {
+    try {
+      const id = "59a2f0667898dca760b01e56";
+      const accessToken = await TestHelper.getToken(SocoboUserRoleType.User, true);
+      const resultAfter = await TestHelper.getAgent()
+        .put(`/api/v1/fooditemcategory/${id}`)
+        .set("x-access-token", accessToken)
+        .set("Content-Type", "application/json")
+        .send({ name: "ha-milk"});
+    } catch (error) {
+      const errorBody = error.response.body;
+      const errorMessage = "The user has unsufficient permission to access the requested resource";
+      expect(errorBody).to.have.deep.property("message", errorMessage);
+      expect(errorBody).to.have.deep.property("method", "checkValidUser(..)");
+      expect(errorBody).to.have.deep.property("source", "AuthValidationMiddleware");
+    }
+  });
+
+  it("POST /api/v1/fooditemcategory should fail to save a new category AS USER", async () => {
+    try {
+      const accessToken = await TestHelper.getToken();
+      const result = await TestHelper.getAgent()
+        .post(`/api/v1/fooditemcategory`)
+        .set("x-access-token", accessToken)
+        .set("Content-Type", "application/json")
+        .send({ name: "choco milk", foodItemId: "59a2ef66b9c6c5139160b4d8" });
+    } catch (error) {
+      const errorBody = error.response.body;
+      const errorMessage = "The user has unsufficient permission to access the requested resource";
+      expect(errorBody).to.have.deep.property("message", errorMessage);
+      expect(errorBody).to.have.deep.property("method", "checkValidUser(..)");
+      expect(errorBody).to.have.deep.property("source", "AuthValidationMiddleware");
+    }
+  });
+
+  it("DELETE /api/v1/fooditemcategory/:id should fail to delete a category AS USER", async () => {
+    try {
+      const id = "59a2f0667898dca760b01e56";
+      const accessToken = await TestHelper.getToken();
+      const result = await TestHelper.getAgent()
+        .del(`/api/v1/fooditemcategory/${id}`)
+        .set("x-access-token", accessToken);
+    } catch (error) {
+      const errorBody = error.response.body;
+      const errorMessage = "The user has unsufficient permission to access the requested resource";
+      expect(errorBody).to.have.deep.property("message", errorMessage);
+      expect(errorBody).to.have.deep.property("method", "checkValidUser(..)");
+      expect(errorBody).to.have.deep.property("source", "AuthValidationMiddleware");
+    }
   });
 });
