a38b35fd50a3521332d49d8019cfc2f2a83fe316
Patrick Hebner <hebner.tec@gmail.com>
2018-01-01 22:24:06 +0100

    #100 add test for recipe ingredients

--- server/sql/locale-demo-data-creation.sh
+++ server/sql/locale-demo-data-creation.sh
@@ -6,7 +6,7 @@ then
   mongoimport --db socobo_dev --collection socobouser --drop --file ./data/create-socobouser.json --jsonArray
   mongoimport --db socobo_dev --collection recipe --drop --file ./data/create-recipe.json --jsonArray
   mongoimport --db socobo_dev --collection recipeCategory --drop --file ./data/create-recipe-categories.json  --jsonArray
-  mongoimport --db socobo_dev --collection recipeIngredient --drop --file ./dev/create-recipe-ingredients.json --jsonArray
+  mongoimport --db socobo_dev --collection recipeIngredient --drop --file ./data/create-recipe-ingredients.json --jsonArray
   mongoimport --db socobo_dev --collection fooditemtemplate --drop --file ./data/create-fooditemtemplate.json --jsonArray
   mongoimport --db socobo_dev --collection fooditemcategory --drop --file ./data/create-fooditemcategory.json --jsonArray
   mongoimport --db socobo_dev --collection fooditemunit --drop --file ./data/create-fooditemunit.json --jsonArray
@@ -18,7 +18,7 @@ then
   mongoimport --db socobo_test --collection socobouser --drop --file ./data/create-socobouser.json --jsonArray
   mongoimport --db socobo_test --collection recipe --drop --file ./data/create-recipe.json --jsonArray
   mongoimport --db socobo_test --collection recipeCategory --drop --file ./data/create-recipe-categories.json  --jsonArray
-  mongoimport --db socobo_test --collection recipeIngredient --drop --file ../dev/create-recipe-ingredients.json --jsonArray
+  mongoimport --db socobo_test --collection recipeIngredient --drop --file ./data/create-recipe-ingredients.json --jsonArray
   mongoimport --db socobo_test --collection fooditemtemplate --drop --file ./data/create-fooditemtemplate.json --jsonArray
   mongoimport --db socobo_test --collection fooditemcategory --drop --file ./data/create-fooditemcategory.json --jsonArray
   mongoimport --db socobo_test --collection fooditemunit --drop --file ./data/create-fooditemunit.json --jsonArray
--- server/test/52-recipe-ingredient.test.ts
+++ server/test/52-recipe-ingredient.test.ts
@@ -19,153 +19,118 @@ describe("RecipeIngredientRoute - API v1", () => {
     expect(result).to.be.status(200);
   });
 
-  // it("GET /api/v1/recipecategory should return all categories", async () => {
-  //   const accessToken = await TestHelper.getToken();
-  //   const result = await TestHelper.getAgent()
-  //     .get("/api/v1/recipecategory")
-  //     .set("x-access-token", accessToken);
-  //   expect(result.body.length).to.equal(3);
-  // });
+  it("GET /api/v1/recipeingredient should return all ingredients", async () => {
+    const accessToken = await TestHelper.getToken();
+    const result = await TestHelper.getAgent()
+      .get("/api/v1/recipeingredient")
+      .set("x-access-token", accessToken);
+    expect(result.body.length).to.equal(3);
+  });
 
-  // it("GET /api/v1/recipecategory/:id should return the category with the provied id", async () => {
-  //   const accessToken = await TestHelper.getToken();
-  //   const result = await TestHelper.getAgent()
-  //     .get("/api/v1/recipecategory/5092ef66b9c6c5139160b4d1")
-  //     .set("x-access-token", accessToken);
-  //   expect(result.body.title).to.equal("Category 1");
-  //   expect(result.body.description).to.equal("Category 1 Desc");
-  //   expect(result.body._id).to.equal("5092ef66b9c6c5139160b4d1");
-  // });
+  it("GET /api/v1/recipeingredient?resolve should return all ingredients with resolved fooditemTemplate", async () => {
+    const accessToken = await TestHelper.getToken();
+    const result = await TestHelper.getAgent()
+      .get("/api/v1/recipeingredient")
+      .query({resolve: true})
+      .set("x-access-token", accessToken);
+    expect(result.body.length).to.equal(3);
+    expect(result.body[0].fooditemTemplate.name).to.equal("bread");
+  });
 
-  // it("POST /api/v1/recipecategory should create a new category", async () => {
-  //   const accessToken = await TestHelper.getToken(SocoboUserRoleType.Admin, true);
+  it("GET /api/v1/recipeingredient/:id should return the ingredient with the provied id", async () => {
+    const accessToken = await TestHelper.getToken();
+    const result = await TestHelper.getAgent()
+      .get("/api/v1/recipeingredient/1234ef66b9c6c5139160b4d1")
+      .set("x-access-token", accessToken);
+    expect(result.body).to.deep.equal({
+      _id : "1234ef66b9c6c5139160b4d1",
+      updatedAt: "2017-08-27T18:48:54.865Z",
+      createdAt: "2017-08-27T18:48:54.865Z",
+      amount: 9,
+      fooditemTemplateId: "59a2eeaaf09a4c43cb92ef0b"});
+  });
 
-  //   const requestBody = {
-  //     description: "Created Recipe 1 Desc",
-  //     title: "Crated Recipe 1"
-  //   };
+  it("GET /api/v1/recipeingredient/:id?resolve should return the ingredient with the provied id and the resolved fooditemtemplate", async () => {
+    const accessToken = await TestHelper.getToken();
+    const result = await TestHelper.getAgent()
+      .get("/api/v1/recipeingredient/1234ef66b9c6c5139160b4d1")
+      .query({resolve: true})
+      .set("x-access-token", accessToken);
+    expect(result.body._id).to.deep.equal("1234ef66b9c6c5139160b4d1");
+    expect(result.body.fooditemTemplate.name).to.deep.equal("bread");
+  });
 
-  //   const result = await TestHelper.getAgent()
-  //     .post("/api/v1/recipecategory")
-  //     .set("x-access-token", accessToken)
-  //     .set("Content-Type", "application/json")
-  //     .send(requestBody);
-  //   expect(result.body.title).to.equal(requestBody.title);
-  //   expect(result.body.description).to.equal(requestBody.description);
-  //   expect(result.body).to.have.property("_id");
-  //   expect(result.body).to.have.property("createdAt");
-  //   expect(result.body).to.have.property("updatedAt");
-  // });
+  it("POST /api/v1/recipeingredient should create a new ingredient", async () => {
+    const accessToken = await TestHelper.getToken(SocoboUserRoleType.User, true);
 
-  // it("POST /api/v1/recipecategory normal user is not permitted to create a category", async () => {
-  //   const accessToken = await TestHelper.getToken(SocoboUserRoleType.User, true);
+    const requestBody = {
+      amount: 111,
+      fooditemTemplateId: "59a2eea98a5a150e9e829409"
+    };
 
-  //   const requestBody = {
-  //     description: "Created Recipe 1 Desc",
-  //     title: "Crated Recipe 1"
-  //   };
+    const result = await TestHelper.getAgent()
+      .post("/api/v1/recipeingredient")
+      .set("x-access-token", accessToken)
+      .set("Content-Type", "application/json")
+      .send(requestBody);
+    expect(result.body.fooditemTemplateId).to.equal(requestBody.fooditemTemplateId);
+    expect(result.body.amount).to.equal(requestBody.amount);
+    expect(result.body).to.have.property("_id");
+    expect(result.body).to.have.property("createdAt");
+    expect(result.body).to.have.property("updatedAt");
+  });
 
-  //   try {
-  //     const result = await TestHelper.getAgent()
-  //       .post("/api/v1/recipecategory")
-  //       .set("x-access-token", accessToken)
-  //       .set("Content-Type", "application/json")
-  //       .send(requestBody);
+  it("PUT /api/v1/recipeingredient/:id should update the ingredient", async () => {
+    const accessToken = await TestHelper.getToken(SocoboUserRoleType.User, true);
 
-  //   } catch (error) {
-  //     expect(error.status).to.be.eql(403);
-  //     const msg = "The user has unsufficient permission to access the requested resource";
-  //     expect(error.response.body).to.have.property("message", msg);
-  //   }
-  // });
+    const requestBody = {
+      amount: 111,
+      fooditemTemplateId: "59a2eea98a5a150e9e829111"
+    };
 
-  // it("PUT /api/v1/recipecategory/:id should update the category", async () => {
-  //   const accessToken = await TestHelper.getToken(SocoboUserRoleType.Admin, true);
+    const resultBefore = await TestHelper.getAgent()
+      .get("/api/v1/recipeingredient/1234ef66b9c6c5139160b4d2")
+      .set("x-access-token", accessToken);
 
-  //   const requestBody = {
-  //     description: "Created Recipe 1 Desc UPDATED",
-  //     title: "Crated Recipe 1 UPDATED"
-  //   };
+    expect(resultBefore.body.amount).to.equal(2);
+    expect(resultBefore.body.fooditemTemplateId).to.equal("59a2eeaaf09a4c43cb92ef0b");
 
-  //   const resultBefore = await TestHelper.getAgent()
-  //     .get("/api/v1/recipecategory/5092ef66b9c6c5139160b4d1")
-  //     .set("x-access-token", accessToken);
+    const result = await TestHelper.getAgent()
+      .put("/api/v1/recipeingredient/1234ef66b9c6c5139160b4d2")
+      .set("x-access-token", accessToken)
+      .set("Content-Type", "application/json")
+      .send(requestBody);
+    expect(result.body.amount).to.equal(requestBody.amount);
+    expect(result.body.fooditemTemplateId).to.equal(requestBody.fooditemTemplateId);
+    expect(result.body._id).to.equal(resultBefore.body._id);
+    expect(result.body.updatedAt).to.be.not.equal(resultBefore.body.updatedAt);
+  });
 
-  //   expect(resultBefore.body.title).to.equal("Category 1");
-  //   expect(resultBefore.body.description).to.equal("Category 1 Desc");
+  it("DELETE /api/v1/recipeingredient/:id should delete the ingredient", async () => {
+    const accessToken = await TestHelper.getToken(SocoboUserRoleType.Admin, true);
 
-  //   const result = await TestHelper.getAgent()
-  //     .put("/api/v1/recipecategory/5092ef66b9c6c5139160b4d1")
-  //     .set("x-access-token", accessToken)
-  //     .set("Content-Type", "application/json")
-  //     .send(requestBody);
-  //   expect(result.body.title).to.equal(requestBody.title);
-  //   expect(result.body.description).to.equal(requestBody.description);
-  //   expect(result.body._id).to.equal(resultBefore.body._id);
-  //   expect(result.body.updatedAt).to.be.not.equal(resultBefore.body.updatedAt);
-  // });
+    const resultBefore = await TestHelper.getAgent()
+      .get("/api/v1/recipeingredient/1234ef66b9c6c5139160b4d3")
+      .set("x-access-token", accessToken);
 
-  // it("PUT /api/v1/recipecategory/:id normal user is not permitted to update a category", async () => {
-  //   const accessToken = await TestHelper.getToken(SocoboUserRoleType.User, true);
+    expect(resultBefore.body.amount).to.equal(5);
+    expect(resultBefore.body.fooditemTemplateId).to.equal("59a2eeaaf09a4c43cb92ef0b");
 
-  //   const requestBody = {
-  //     description: "Created Recipe 1 Desc UPDATED",
-  //     title: "Crated Recipe 1 UPDATED"
-  //   };
+    const result = await TestHelper.getAgent()
+      .del("/api/v1/recipeingredient/1234ef66b9c6c5139160b4d3")
+      .set("x-access-token", accessToken);
+    expect(result.status).to.equal(200);
 
-  //   try {
-  //     const result = await TestHelper.getAgent()
-  //       .put("/api/v1/recipecategory/5092ef66b9c6c5139160b4d1")
-  //       .set("x-access-token", accessToken)
-  //       .set("Content-Type", "application/json")
-  //       .send(requestBody);
-
-  //   } catch (error) {
-  //     expect(error.status).to.be.eql(403);
-  //     const msg = "The user has unsufficient permission to access the requested resource";
-  //     expect(error.response.body).to.have.property("message", msg);
-  //   }
-  // });
-
-  // it("DELETE /api/v1/recipecategory/:id should delete the category", async () => {
-  //   const accessToken = await TestHelper.getToken(SocoboUserRoleType.Admin, true);
-
-  //   const resultBefore = await TestHelper.getAgent()
-  //     .get("/api/v1/recipecategory/5092ef66b9c6c5139160b4d3")
-  //     .set("x-access-token", accessToken);
-
-  //   expect(resultBefore.body.title).to.equal("Category 3");
-  //   expect(resultBefore.body.description).to.equal("Category 3 Desc");
-
-  //   const result = await TestHelper.getAgent()
-  //     .del("/api/v1/recipecategory/5092ef66b9c6c5139160b4d3")
-  //     .set("x-access-token", accessToken);
-  //   expect(result.status).to.equal(200);
-
-  //   try {
-  //     await TestHelper.getAgent()
-  //       .get("/api/v1/recipecategory/5092ef66b9c6c5139160b4d3")
-  //       .set("x-access-token", accessToken);
-  //   } catch (error) {
-  //     expect(error.status).to.be.eql(404);
-  //     const msg = "Recipe category with ID 5092ef66b9c6c5139160b4d3 could not be found";
-  //     expect(error.response.body).to.have.property("message", msg);
-  //   }
-  // });
-
-  // it("DELETE /api/v1/recipecategory/:id normal user is not permitted to delete a category", async () => {
-  //   const accessToken = await TestHelper.getToken(SocoboUserRoleType.User, true);
-
-  //   try {
-  //     const result = await TestHelper.getAgent()
-  //       .del("/api/v1/recipecategory/5092ef66b9c6c5139160b4d3")
-  //       .set("x-access-token", accessToken);
-  //   } catch (error) {
-  //     expect(error.status).to.be.eql(403);
-  //     const msg = "The user has unsufficient permission to access the requested resource";
-  //     expect(error.response.body).to.have.property("message", msg);
-  //   }
-  // });
+    try {
+      await TestHelper.getAgent()
+        .get("/api/v1/recipeingredient/1234ef66b9c6c5139160b4d3")
+        .set("x-access-token", accessToken);
+    } catch (error) {
+      expect(error.status).to.be.eql(404);
+      const msg = "Recipe ingredient with ID 1234ef66b9c6c5139160b4d3 could not be found";
+      expect(error.response.body).to.have.property("message", msg);
+    }
+  });
 });
 
 /*tslint:enable:no-unused-expression*/
