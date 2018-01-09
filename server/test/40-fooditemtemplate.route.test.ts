19edf6b97820348eaf3247b94607222a0aad9a9e
flashback2k14 <feedbackcalcest@gmail.com>
2018-01-09 22:50:38 +0100

    #95 - add template resolve tests

--- server/test/40-fooditemtemplate.route.test.ts
+++ server/test/40-fooditemtemplate.route.test.ts
@@ -20,10 +20,28 @@ describe("FoodItemTemplateRoute - API v1", () => {
     expect(result.body.length).to.equal(3);
   });
 
+  it("GET /api/v1/fooditemtemplate?resolve should return all templates w/ resolved categories and unit", async () => {
+    const accessToken = await TestHelper.getToken();
+    const result = await TestHelper.getAgent()
+      .get("/api/v1/fooditemtemplate?resolve")
+      .set("x-access-token", accessToken);
+    expect(result.body.length).to.equal(3);
+  });
+
   it("GET /api/v1/fooditemtemplate/:id should return one template", async () => {
     const id = "59a2eea98a5a150e9e829409";
     const accessToken = await TestHelper.getToken();
     const result = await TestHelper.getAgent().get(`/api/v1/fooditemtemplate/${id}`).set("x-access-token", accessToken);
+    expect(result.body).to.not.null;
+  });
+
+  it(`GET /api/v1/fooditemtemplate/:id?resolve should return one template
+      w/ resolved categories and unit`, async () => {
+    const id = "59a2eea98a5a150e9e829409";
+    const accessToken = await TestHelper.getToken();
+    const result = await TestHelper.getAgent()
+      .get(`/api/v1/fooditemtemplate/${id}?resolve`)
+      .set("x-access-token", accessToken);
     expect(result.body).to.not.null;
   });
 
@@ -42,6 +60,39 @@ describe("FoodItemTemplateRoute - API v1", () => {
       expect(result.body).to.deep.property("name");
       expect(result.body).to.deep.property("created");
       expect(result.body).to.deep.property("lastModified");
+  });
+
+  it(`GET /api/v1/fooditemtemplate/:id?resolve should return one template
+      w/ id, categories object array, unit object, name, created & lastModified property`,
+    async () => {
+      const id = "59a2eea98a5a150e9e829409";
+      const accessToken = await TestHelper.getToken();
+      const result = await TestHelper.getAgent()
+        .get(`/api/v1/fooditemtemplate/${id}?resolve`)
+        .set("x-access-token", accessToken);
+
+      expect(result.body).to.deep.property("id");
+      expect(result.body).to.deep.property("name");
+      expect(result.body).to.deep.property("created");
+      expect(result.body).to.deep.property("lastModified");
+
+      expect(result.body).to.not.deep.property("categoryIds");
+      expect(result.body).to.deep.property("categories");
+      expect(result.body.categories).to.be.an.instanceOf(Array);
+      expect(result.body.categories).length(2);
+      expect(result.body.categories[0]).to.be.an.instanceOf(Object);
+      expect(result.body.categories[0]).to.deep.property("id");
+      expect(result.body.categories[0]).to.deep.property("name");
+      expect(result.body.categories[0]).to.deep.property("created");
+      expect(result.body.categories[0]).to.deep.property("lastModified");
+
+      expect(result.body).to.not.deep.property("unitId");
+      expect(result.body).to.deep.property("unit");
+      expect(result.body.unit).to.be.an.instanceOf(Object);
+      expect(result.body.unit).to.deep.property("id");
+      expect(result.body.unit).to.deep.property("name");
+      expect(result.body.unit).to.deep.property("created");
+      expect(result.body.unit).to.deep.property("lastModified");
   });
 
   it("PUT /api/v1/fooditemtemplate/:id should update the template and return the modified", async () => {
